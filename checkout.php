cc365c9a68976ad720fd1c8c84b749b22e762d72
Kevin Putra Tjahjono <91048002+kevinputratjahjono@users.noreply.github.com>
2022-11-28 20:54:49 +0700

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Penyewaan Barang Elektronik</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid">
+        <div class="row bg-secondary py-1 px-xl-5">
+            <div class="col-lg-6 d-none d-lg-block">
+                <div class="d-inline-flex align-items-center h-100">
+                    <a class="text-body mr-3" href="">About</a>
+                    <a class="text-body mr-3" href="">Contact</a>
+                    <a class="text-body mr-3" href="">Help</a>
+                    <a class="text-body mr-3" href="">FAQs</a>
+                </div>
+            </div>
+                <div class="d-inline-flex align-items-center d-block d-lg-none">
+                    <a href="" class="btn px-0 ml-2">
+                        <i class="fas fa-heart text-dark"></i>
+                        <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
+                    </a>
+                    <a href="" class="btn px-0 ml-2">
+                        <i class="fas fa-shopping-cart text-dark"></i>
+                        <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
+            <div class="col-lg-4">
+                <a href="" class="text-decoration-none">
+                    <span class="h1 text-uppercase text-primary bg-dark px-2">Multi</span>
+                    <span class="h1 text-uppercase text-dark bg-primary px-2 ml-n1">Shop</span>
+                </a>
+            </div>
+            <div class="col-lg-4 col-6 text-left">
+                <form action="">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for products">
+                        <div class="input-group-append">
+                            <span class="input-group-text bg-transparent text-primary">
+                                <i class="fa fa-search"></i>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-4 col-6 text-right">
+                <p class="m-0">Customer Service</p>
+                <h5 class="m-0">+012 345 6789</h5>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-dark mb-30">      
+			 <div class="col-lg-9">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">                   
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto py-0">
+                            <a href="./" class="nav-item nav-link">Home</a>                            
+                        </div>                       
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+    <!-- Checkout Start -->
+    <div class="container-fluid">
+        <div class="row px-xl-5">
+            <div class="col-lg-8">
+                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Form Penyewaan</span></h5>
+                <div class="bg-light p-30 mb-5">
+                    <form method="POST" action="checkout2.php"
+					<div class="row">					
+                        <div class="col-md-6 form-group">
+                            <label>Nama Lengkap Sesuai KTP</label>
+                            <input class="form-control" type="text" placeholder="Andra" name="inputname">
+                        </div>                                                
+                        <div class="col-md-6 form-group">
+                            <label>No HP Aktif</label>
+                            <input class="form-control" type="text" placeholder="+62831234567" name="inputphone">
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label>Alamat</label>
+                            <input class="form-control" type="text" placeholder="Jl. Pisang RT 04 Rw 05, Kelapa Dua, Kebon Jeruk 11550" name="inputname">
+                        </div>
+						<div class="col-md-6 form-group">                            
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label>Tanggal Mulai Sewa</label>
+                            <input class="form-control" type="text" placeholder="09-11-2022">
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label>Lama Hari</label>
+                            <input class="form-control" type="text" placeholder="3">
+                        </div>						
+						<div class="bg-light p-30">
+							<button class="btn btn-block btn-primary font-weight-bold py-3">Pesan</button>
+						</div>                
+                        </div>     						
+                    </div>
+                </div>				
+            </div>   			
+        </div>
+    </div>
+    <!-- Checkout End -->
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-secondary mt-5 pt-5">
+        <div class="row px-xl-5 pt-5">
+            <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
+                <h5 class="text-secondary text-uppercase mb-4">Get In Touch</h5>
+                <p class="mb-4">Kami melayani penyewaan barang elektronik di daerah sekitar Jakarta.</p>
+                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Jl. Letjen S. Parman No.1, RT.6/RW.16, Grogol, Kec. Grogol petamburan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11440</p>
+                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>singa@trisakti.ac.id</p>
+                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+62 1234 5678</p>
+            </div>
+            <div class="col-lg-8 col-md-12">
+                <div class="row">
+                    <div class="col-md-4 mb-5">
+                        <h5 class="text-secondary text-uppercase mb-4">Quick Shop</h5>
+                        <div class="d-flex flex-column justify-content-start">
+                            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Beranda</a>
+                            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Toko Kami</a>
+                            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Detail Produk</a>
+                            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Keranjang</a>
+                            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+                            <a class="text-secondary" href="#"><i class="fa fa-angle-right mr-2"></i>Hubungi Kami</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-5">
+                        <h5 class="text-secondary text-uppercase mb-4">Berlangganan</h5>
+                        <p>Dapatkan berbagai info menarik</p>
+                        <form action="">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Alamat E-mail">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary">Daftar</button>
+                                </div>
+                            </div>
+                        </form>
+                        <h6 class="text-secondary text-uppercase mt-4 mb-3">Ikuti Kami</h6>
+                        <div class="d-flex">
+                            <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a class="btn btn-primary btn-square" href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
+            <div class="col-md-6 px-xl-0">
+                <p class="mb-md-0 text-center text-md-left text-secondary">
+                    &copy; <a class="text-primary" href="#">Kelompok Singa</a>. All Rights Reserved.
+                    <a class="text-primary" href="https://htmlcodex.com"></a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ inputdb.sql
@@ -0,0 +1,19 @@
+INSERT INTO `barang`(`id_barang`, `nama`, `brand`, `type`, `harga`, `icon_url`, `av_quantity`, `desc`) VALUES 
+('OF001',
+'HP 14s-fq0591AU',
+'HP',
+'office',
+'1000000',
+'OF001.jpg',
+'5',
+'Laptop yang cocok untuk kerja, cocok untuk kamu yang kuliah. Lebih lengkap: https://www.hp.com/id-id/shop/hp-laptop-14s-fq0591au-798d9pa.html')
+
+INSERT INTO `barang`(`id_barang`, `nama`, `brand`, `type`, `harga`, `icon_url`, `av_quantity`, `desc`) VALUES 
+('OF002',
+'Lenovo Slim D330 Flex',
+'Lenovo',
+'office',
+'650000',
+'OF002.jpg',
+'6',
+'Laptop yang cocok untuk kerja, cocok untuk kamu yang kuliah. Lebih lengkap: https://www.lenovo.com/id/in/laptops/ideapad/ideapad-d-series/IdeaPad-D330/p/88IPMX31047')
