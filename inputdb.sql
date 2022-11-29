f3660d94b91748f1a2c5a729157a08131539dfab
JackyLeg <91046882+JackyLeg@users.noreply.github.com>
2022-11-29 13:13:16 +0700

    update sedikit

--- inputdb.sql
+++ inputdb.sql
@@ -28,3 +28,23 @@ INSERT INTO `barang`(`id_barang`, `nama`, `brand`, `type`, `harga`, `icon_url`, 
 'OF003.jpg',
 '',
 '')
+
+INSERT INTO `barang`(`id_barang`, `nama`, `brand`, `type`, `harga`, `icon_url`, `av_quantity`, `desc`) VALUES 
+('AU002',
+'OASE - Wireless Heaphone KG10',
+'OASE',
+'audio',
+'650000',
+'AU001.jpg',
+'1',
+'Headphone dengan suara bass yang terdengar jelas. Lebih lengkap: https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiN2eql29L7AhXySmwGHU-QDh4QFnoECAoQAQ&url=https%3A%2F%2Fmyhartono.com%2Fen%2Foase-wireless-headphone-kg10-series%2F&usg=AOvVaw29MwonvPgoi8dZ-fodkNem')
+
+INSERT INTO `barang`(`id_barang`, `nama`, `brand`, `type`, `harga`, `icon_url`, `av_quantity`, `desc`) VALUES 
+('OF001',
+'HP 14s-fq0591AU',
+'HP',
+'office',
+'1000000',
+'OF001.jpg',
+'5',
+'Laptop yang cocok untuk kerja, cocok untuk kamu yang kuliah. Lebih lengkap: https://www.hp.com/id-id/shop/hp-laptop-14s-fq0591au-798d9pa.html')
